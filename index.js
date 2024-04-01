d121903ae52dcab3e54e2923524034cfd9f49449
LuizFelipeSouzaSilva <141966018+LuizFelipeSouzaSilva@users.noreply.github.com>
2024-04-01 18:58:36 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function abrirsm(){
+    document.getElementById('sobremim').style.display = 'block';
+}
+function fecharsm(){
+    document.getElementById('sobremim').style.display =  'none';
+}
+function abrirf(){
+    document.getElementById('formacao').style.display = 'block';
+}
+function fecharf(){
+    document.getElementById('formacao').style.display =  'none';
+}
+function abrirp(){
+    document.getElementById('portifolio').style.display = 'block';
+}
+function fecharp(){
+    document.getElementById('portifolio').style.display =  'none';
+}
+function abrirc(){
+    document.getElementById('contato').style.display = 'block';
+}
+function fecharc(){
+    document.getElementById('contato').style.display =  'none';
+}
